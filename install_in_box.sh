181751593abe838f541f25962083701a7c9e8a3c
fred4jupiter <hamsterhase@gmx.de>
2015-04-26 14:28:31 +0200

    cleanup

--- install_in_box.sh
+++ install_in_box.sh
@@ -4,6 +4,3 @@ sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update -y
 sudo apt-get install -y software-properties-common ansible git
 
-# git clone https://github.com/fred4jupiter/graylog2-in-a-box.git /home/vagrant/graylog2-in-a-box
-# cd /home/vagrant/graylog2-in-a-box/provisioning
-# sudo ansible-playbook -i inventory playbook.yml
